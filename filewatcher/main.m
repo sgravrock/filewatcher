21c7654d43f0f26ad5d6bf64ab31baf62fc039d9
Steve Gravrock <sdg@panix.com>
2016-02-14 10:46:11 -0800

    Optionally write output as JSON on a single line
    For easier processing by other tools in a pipeline.

--- filewatcher/main.m
+++ filewatcher/main.m
@@ -1,12 +1,21 @@
 #import <Foundation/Foundation.h>
 
+struct options {
+	BOOL json;
+};
+
+static struct options parseArgs(const char *argv[]);
 static void cb(ConstFSEventStreamRef streamRef, void *clientCallBackInfo, size_t numEvents, void *eventPaths, const FSEventStreamEventFlags eventFlags[], const FSEventStreamEventId eventIds[]);
 static BOOL isInteresting(FSEventStreamEventFlags flags);
+static void writePaths(NSArray *paths);
+static void writeJson(NSArray *paths);
 
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
+		struct options opts = parseArgs(argv);
+		struct FSEventStreamContext context = { 0, &opts, NULL, NULL, NULL };
 		NSString *dir = @".";
-		FSEventStreamRef stream = FSEventStreamCreate(NULL, cb, NULL, (__bridge CFArrayRef)(@[dir]), kFSEventStreamEventIdSinceNow, 1, kFSEventStreamCreateFlagUseCFTypes | kFSEventStreamCreateFlagFileEvents);
+		FSEventStreamRef stream = FSEventStreamCreate(NULL, cb, &context, (__bridge CFArrayRef)(@[dir]), kFSEventStreamEventIdSinceNow, 1, kFSEventStreamCreateFlagUseCFTypes | kFSEventStreamCreateFlagFileEvents);
 		FSEventStreamScheduleWithRunLoop(stream, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
 		FSEventStreamStart(stream);
 		
@@ -16,13 +25,37 @@ int main(int argc, const char * argv[]) {
 	return EXIT_FAILURE; // can't happen
 }
 
+static struct options parseArgs(const char *argv[]) {
+	struct options opts = { NO };
+	
+	for (int i = 1; argv[i] != NULL; i++) {
+		if (strcmp(argv[i], "--json") == 0) {
+			opts.json = YES;
+		} else {
+			fprintf(stderr, "Usage: %s [--json]\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	
+	return opts;
+}
+
 static void cb(ConstFSEventStreamRef streamRef, void *clientCallBackInfo, size_t numEvents, void *eventPaths, const FSEventStreamEventFlags eventFlags[], const FSEventStreamEventId eventIds[]) {
+	
+	NSMutableArray *interestings = [NSMutableArray array];
 	
 	for (size_t i = 0; i < numEvents; i++) {
 		if (isInteresting(eventFlags[i])) {
 			NSString *path = CFArrayGetValueAtIndex(eventPaths, i);
-			puts([[NSString stringWithFormat:@"%@", path] UTF8String]);
+			[interestings addObject:path];
 		}
+	}
+	
+	
+	if (((struct options *)clientCallBackInfo)->json) {
+		writeJson(interestings);
+	} else {
+		writePaths(interestings);
 	}
 }
 
@@ -36,4 +69,25 @@ static BOOL isInteresting(FSEventStreamEventFlags flags) {
 	}
 	
 	return NO;
+}
+
+static void writePaths(NSArray *paths) {
+	for (NSString *path in paths) {
+		puts([path UTF8String]);
+	}
+}
+
+static void writeJson(NSArray *paths) {
+	NSError *error = nil;
+	NSData *data = [NSJSONSerialization dataWithJSONObject:paths
+												   options:0
+													 error:&error];
+	
+	if (data) {
+		[data writeToFile:@"/dev/stdout" atomically:NO];
+		putchar('\n');
+	} else {
+		fprintf(stderr, "Error serializing file paths: %s\n",
+				[[error localizedDescription] UTF8String]);
+	}
 }
